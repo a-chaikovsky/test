2475bc5c1360794b658b4adb02ecc5d119894a07
arthur <arthur.chaikovsky@gmail.com>
2019-04-27 05:02:10 +0300

    test 3

---
+++ test 3/javascript.js
@@ -0,0 +1,12 @@
+$('#burg').on('click', function () {
+    $('.nav-menu').slideToggle(700);
+});
+
+$('#showForm').on('click', function () {
+    $('.form').slideToggle(500);
+});
+
+$('#showSearch').on('click', function () {
+    $('.search').slideToggle(500);
+});
+
